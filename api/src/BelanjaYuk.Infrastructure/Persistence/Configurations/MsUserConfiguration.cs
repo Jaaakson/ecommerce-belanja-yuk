2fea0eb8d1d3b5419846039c8baab3f7a5da46f6
Jakson <jaksonrichard34@gmail.com>
2026-08-02 12:38:48 +0700

    feat(infrastructure): add master entity configurations

---
+++ api/src/BelanjaYuk.Infrastructure/Persistence/Configurations/MsUserConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using BelanjaYuk.Domain.Entities;
+using BelanjaYuk.Infrastructure.Persistence.Extensions;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BelanjaYuk.Infrastructure.Persistence.Configurations;
+
+public class MsUserConfiguration : IEntityTypeConfiguration<MsUser>
+{
+    public void Configure(EntityTypeBuilder<MsUser> builder)
+    {
+        builder.ToTable("MsUser");
+
+        builder.HasKey(e => e.IdUser);
+
+        builder.Property(e => e.IdUser).HasColumnType("nvarchar(36)").ValueGeneratedNever();
+        builder.Property(e => e.UserName).HasColumnType("nvarchar(100)").IsRequired();
+        builder.Property(e => e.Email).HasColumnType("nvarchar(100)").IsRequired();
+        builder.Property(e => e.PhoneNumber).HasColumnType("nvarchar(50)").IsRequired();
+        builder.Property(e => e.FirstName).HasColumnName("Firstname").HasColumnType("nvarchar(100)").IsRequired();
+        builder.Property(e => e.LastName).HasColumnType("nvarchar(200)");
+        builder.Property(e => e.DOB).HasColumnType("datetime2");
+        builder.Property(e => e.IdGender).HasColumnType("nvarchar(36)").IsRequired();
+
+        builder.HasIndex(e => e.UserName).IsUnique();
+        builder.HasIndex(e => e.Email).IsUnique();
+        builder.HasIndex(e => e.PhoneNumber).IsUnique();
+
+        builder.HasOne(e => e.Gender)
+            .WithMany()
+            .HasForeignKey(e => e.IdGender)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.ConfigureAuditColumns();
+        builder.HasQueryFilter(e => e.IsActive);
+    }
+}
---
+++ api/src/BelanjaYuk.Infrastructure/Persistence/Configurations/MsUserPasswordConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using BelanjaYuk.Domain.Entities;
+using BelanjaYuk.Infrastructure.Persistence.Extensions;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BelanjaYuk.Infrastructure.Persistence.Configurations;
+
+public class MsUserPasswordConfiguration : IEntityTypeConfiguration<MsUserPassword>
+{
+    public void Configure(EntityTypeBuilder<MsUserPassword> builder)
+    {
+        builder.ToTable("MsUserPassword");
+
+        builder.HasKey(e => e.IdUserPassword);
+
+        builder.Property(e => e.IdUserPassword).HasColumnType("nvarchar(36)").ValueGeneratedNever();
+        builder.Property(e => e.IdUser).HasColumnType("nvarchar(36)").IsRequired();
+        builder.Property(e => e.PasswordHash).HasColumnType("nvarchar(200)").IsRequired();
+
+        builder.HasOne(e => e.User)
+            .WithOne(u => u.Password)
+            .HasForeignKey<MsUserPassword>(e => e.IdUser)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.ConfigureAuditColumns();
+        builder.HasQueryFilter(e => e.IsActive);
+    }
+}
