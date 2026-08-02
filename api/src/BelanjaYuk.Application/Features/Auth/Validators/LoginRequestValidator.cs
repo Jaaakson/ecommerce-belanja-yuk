1cf5be5b16b8195129fefa67529542f5584d2878
Jakson <jaksonrichard34@gmail.com>
2026-08-02 13:57:27 +0700

    feat(auth): add request validators and application exception types

---
+++ api/src/BelanjaYuk.Application/Features/Auth/Validators/LoginRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using BelanjaYuk.Application.Features.Auth.Dtos;
+
+using FluentValidation;
+
+namespace BelanjaYuk.Application.Features.Auth.Validators;
+
+public class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Identifier)
+            .NotEmpty().WithMessage("Email atau nomor HP wajib diisi.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Kata sandi wajib diisi.");
+    }
+}
