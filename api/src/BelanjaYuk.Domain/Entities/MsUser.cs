3651381f39cc980c7df8522b32899fc33e6ed9df
Jakson <jaksonrichard34@gmail.com>
2026-08-02 11:44:36 +0700

    feat(domain): add user and product master entities

---
+++ api/src/BelanjaYuk.Domain/Entities/MsUser.cs
@@ -0,0 +1,18 @@
+﻿using BelanjaYuk.Domain.Common;
+
+namespace BelanjaYuk.Domain.Entities;
+
+public class MsUser : AuditableEntity
+{
+    public string IdUser { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string? LastName { get; set; }
+    public DateTime? DOB { get; set; }
+    public string IdGender { get; set; } = null!;
+
+    public LtGender? Gender { get; set; }
+    public MsUserPassword? Password { get; set; }
+}
